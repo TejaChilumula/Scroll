9ad7593838bf33fc558d6d30380bbcc3aec00fba
Teja Chilumula <tejachilumula07@gmail.com>
2023-11-05 21:54:48 -0800

    Intial Home page with side bar and Header

---
+++ scroll/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Header from '@/components/Header'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+   <>
+    <Header label='Home' />
+   </> 
+    )
+}
